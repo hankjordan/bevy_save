9a6f6db98f94a100b6985b7cec23a76fb71f32a4
hank <hank@hank.co.in>
2023-01-14 10:38:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Bevy_save
+[![][img_bevy]][bevy] [![][img_version]][crates] [![][img_doc]][doc] [![][img_license]][license] [![][img_tracking]][tracking] [![][img_downloads]][crates]
+
+A framework for saving and loading game state in Bevy.
+
+## License
+
+`bevy_save` is dual-licensed under MIT and Apache-2.0.
+
+## Compatibility
+
+NOTE: We do not track Bevy main.
+
+|Bevy Version|Crate Version          |
+|------------|-----------------------|
+|`0.9`       |`0.1`                  |
+
+[img_bevy]: https://img.shields.io/badge/Bevy-0.9-blue
+[img_version]: https://img.shields.io/crates/v/bevy_save.svg
+[img_doc]: https://docs.rs/bevy_save/badge.svg
+[img_license]: https://img.shields.io/badge/license-MIT%2FApache-blue.svg
+[img_downloads]:https://img.shields.io/crates/d/bevy_save.svg
+[img_tracking]: https://img.shields.io/badge/Bevy%20tracking-released%20version-lightblue
+
+[bevy]: https://crates.io/crates/bevy/0.9.1
+[crates]: https://crates.io/crates/bevy_save
+[doc]: https://docs.rs/bevy_save/
+[license]: https://github.com/hankjordan/bevy_save#license
+[tracking]: https://github.com/bevyengine/bevy/blob/main/docs/plugins_guidelines.md#main-branch-tracking
---
+++ examples/main.rs
@@ -0,0 +1,108 @@
+use bevy::prelude::*;
+use bevy_save::prelude::*;
+
+#[derive(Resource, Reflect, Default, Debug)]
+#[reflect(Resource)]
+pub struct Balance {
+    amount: usize,
+}
+
+#[derive(Component, Reflect, Default, Debug)]
+#[reflect(Component)]
+pub struct Health {
+    amount: f32,
+}
+
+#[derive(Component, Reflect, Default)]
+#[reflect(Component)]
+pub struct Player;
+
+fn setup(
+    mut commands: Commands,
+) {
+    commands
+        .spawn((
+            SpatialBundle::default(),
+            Health { amount: 25.0 },
+            Player,
+        ));
+}
+
+fn damage(
+    buttons: Res<Input<MouseButton>>,
+    mut players: Query<&mut Health, With<Player>>,
+) {
+    if buttons.just_released(MouseButton::Left) {
+        for mut health in &mut players {
+            health.amount -= 1.0;
+        }
+    }
+}
+
+fn heal(
+    buttons: Res<Input<MouseButton>>,
+    mut balance: ResMut<Balance>,
+    mut players: Query<&mut Health, With<Player>>,
+) {
+    if buttons.just_released(MouseButton::Right) && balance.amount > 0 {
+        for mut health in &mut players {
+            health.amount += 1.0;
+        }
+
+        balance.amount -= 1;
+    }
+}
+
+fn status(
+    balance: Res<Balance>,
+    players: Query<(Entity, &Health), Changed<Health>>,
+) {
+    if balance.is_changed() {
+        info!("Balance: {:?} gp", balance.amount);
+    }
+
+    for (entity, health) in &players {
+        info!("{:?}: {:?} hp", entity, health.amount);
+    }
+}
+
+fn savestate(
+    world: &mut World,
+) {
+    let keys = world.resource::<Input<KeyCode>>();
+
+    if keys.just_released(KeyCode::Space) {
+        info!("Checkpoint");
+        world.checkpoint();
+    } else if keys.just_released(KeyCode::Left) {
+        info!("Rollback");
+        world.rollback(1);
+    } else if keys.just_released(KeyCode::Right) {
+        info!("Rollforward");
+        world.rollback(-1);
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.build().set(AssetPlugin {
+            asset_folder: "examples/assets".to_owned(),
+            ..default()
+        }))
+
+        .insert_resource(Balance { amount: 42 })
+
+        .register_saveable::<Balance>()
+        .register_saveable::<Health>()
+        .register_saveable::<Player>()
+
+        .add_startup_system(setup)
+
+        .add_system(damage)
+        .add_system(heal)
+        .add_system(status)
+
+        .add_system(savestate)
+
+        .run();
+}
